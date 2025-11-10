805031a119ce09b0c88b6fc31bb5f3163aebf657
JIMIN JANG <jmjang2122@gmail.com>
2025-11-10 14:26:53 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.42 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,13 +1,11 @@
 def solution(A,B):
     answer = 0
     A.sort()
-    B.sort()
-    sum1=0
-    sum2=0
+    B.sort(reverse=True)
     for i in range(len(A)):
-        sum1+=A[i]*B[i-(1+(2*i))]
-        sum2+=A[i-(1+(2*i))]*B[i]
+        answer+=A[i]*B[i]
+
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     print('Hello Python')
 
-    return min(sum1,sum2)
+    return answer
